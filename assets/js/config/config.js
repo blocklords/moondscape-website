e25cf6209172bea024cd268aae0f75cf470312e7
LevanOzashvili <ozashvili1@gmail.com>
2022-03-14 15:56:40 +0400

    Add pre config values.

--- assets/js/config/config.js
+++ assets/js/config/config.js
@@ -1,10 +1,10 @@
 window.config = {
   1284: {
-    "CityNft": "",
-    "MscpToken": "",
-    "MoonscapeBeta": "",
-    "amount": 100000,
-    "api": "https://api.moonscapegame.com/api/v1/city/find-by-nft-ids"
+    "CityNft": "0x7cdc5d0188733edf08412eecb9afa840772615dc",
+    "MscpToken": "0xB60590313975f0d98821B6Cab5Ea2a6d9641D7B6",
+    "MoonscapeBeta": "0x82d9DD4838F0765eAC6877b0D75C5Da45E8A43Bb",
+    "amount": 3000,
+    "api": "https://pre-api.moonscapegame.com/api/v1/city/find-by-nft-ids"
   },
   1287: {
     "CityNft": "0xffa086A32259be131b6a643D034512881A6B0f51",

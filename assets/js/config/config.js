fab2bfb650fc39d19b9ed92e05dcb6c4a4eea4bf
LevanOzashvili <ozashvili1@gmail.com>
2022-04-08 13:31:15 +0400

    fix stake mscp amount.

--- assets/js/config/config.js
+++ assets/js/config/config.js
@@ -3,14 +3,12 @@ window.config = {
     "CityNft": "0x7cdc5d0188733edf08412eecb9afa840772615dc",
     "MscpToken": "0xB60590313975f0d98821B6Cab5Ea2a6d9641D7B6",
     "MoonscapeBeta": "0x2aF02f0EbE994d21848146b8f934F180A38de64c",
-    "amount": 3000,
     "api": "https://pre-api.moonscapegame.com/api/v1/city/"
   },
   1287: {
     "CityNft": "0xffa086A32259be131b6a643D034512881A6B0f51",
     "MscpToken": "0xF2C84Cb3d1e9Fac001F36c965260aA2a9c9D822D",
     "MoonscapeBeta": "0x24C5d89298bC871608D2089074E4111E306eabfb",
-    "amount": 100000,
     "api": "https://beta-api.moonscapegame.com/api/v1/city/"
   }
 }

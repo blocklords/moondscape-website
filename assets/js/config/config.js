7db5693b3d813bf64c7b804f587df0d6cd950878
LevanOzashvili <ozashvili1@gmail.com>
2022-04-14 14:50:18 +0400

    change mainnet api url.

--- assets/js/config/config.js
+++ assets/js/config/config.js
@@ -3,7 +3,7 @@ window.config = {
     "CityNft": "0x7cdc5d0188733edf08412eecb9afa840772615dc",
     "MscpToken": "0xB60590313975f0d98821B6Cab5Ea2a6d9641D7B6",
     "MoonscapeBeta": "0x49578a0ceCBA3389187AabD59CdFFDA02e151c04",
-    "api": "https://pre-api.moonscapegame.com/api/v1/city/"
+    "api": "https://api.moonscapegame.com/api/v1/city/"
   },
   1287: {
     "CityNft": "0xffa086A32259be131b6a643D034512881A6B0f51",
